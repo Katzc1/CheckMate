f1cebb4f3aa36523253bbcd6c8c31b2472a02a95
Kai Weinstein <weinsteinj2@wit.edu>
2026-01-15 22:24:17 -0500

    Everything done

--- CheckMate.java
+++ CheckMate.java
@@ -3,7 +3,11 @@
 public class CheckMate {
 
 	public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("This is a highly functional program with no probems");
+    runGame();
   }
 
+	public static void runGame() {
+		System.out.print("Pretend ts is Chess twin <3");
+	}
 }
