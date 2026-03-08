1abf2795391e900de37cfa0427bc78a77e2754e4
Kai Weinstein <weinsteinj2@wit.edu>
2026-03-08 19:28:36 -0400

    PLEASE GOD WORK PLEASE

--- Piece.java
+++ Piece.java
@@ -1,9 +1,55 @@
+//This class has been made abstract because there isn't a use case for a generic "Piece"
 
-public class Piece {
+//The pieces on the board will always be child pieces such as pawns and rooks
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public abstract class Piece {
 
-	}
+//We define attributes every piece should have, and set the default color of the piece to Black
+
+public String Color = "Black";
+
+public double spacesToMove;
+
+public Square Location;
+
+//Constructor
+
+public Piece(String col, double numSpc, Square loc) {
+
+this.Color = col;
+
+this.spacesToMove = numSpc;
+
+this.Location = loc;
+
+}
+
+//Because the method is abstract, we can say that each child has to decide for itself
+
+//how the method works. Because there is no universal legality for every piece we
+
+//leave this as a placeholder until the child classes define it.
+
+public abstract boolean checkLegalMove(Square destination);
+
+//Other methods that each and every piece should be able to do.
+
+public void setLocation(Square destination) {
+
+Location = destination;
+
+}
+
+public Square getLocation() {
+
+return Location;
+
+}
+
+public String getColor() {
+
+return Color;
+
+}
 
 }
--- Square.java
+++ Square.java
@@ -1,35 +1,154 @@
 
 public class Square {
 
-	
-		private int row;
-		private int col;
-		private Piece piece;
-		
-	public Square(int row, int col) {
-		this.row = row;
-		this.col = col;
-		
-	}
-	
-	public int getRow() {
-		return row;
-	}
-	
-	public int getCol() {
-		return col;
-	}
+//Rank and file: coordinates as to where the square is
 
-	public Piece getPiece() {
-		return piece;
-	}
-	
-	public void setPiece(Piece piece) {
-		this.piece = piece;
-	}
-	public boolean isEmpty() {
-		return piece == null;
-	}
+public int rank;
+
+//In this case both are represented as integers while the names of the squares denote
+
+//Their actual algebraic chess notation (e5 vs 5 5)
+
+public int file;
+
+public String Color;
+
+//Having a boolean to check if the square is occupied rather than relying on
+
+//Whether or not onSquare is null is safer. Don't really want to mess with null and boolean
+
+//checks are easier than checking if it is equal to null
+
+public boolean isOccupied;
+
+public Piece onSquare;
+
+//Constructor for when the square is empty (technically dont need the bool in this case
+
+//But kept for clarity
+
+public Square(int row, int column, String col, boolean occ) {
+
+this.rank = row;
+
+this.file = column;
+
+this.Color = col;
+
+this.isOccupied = occ;
+
 }
 
+//Constructor for when the square is occupied
 
+public Square(int row, int column, String col, boolean occ, Piece p) {
+
+this.rank = row;
+
+this.file = column;
+
+this.Color = col;
+
+this.isOccupied = occ;
+
+this.onSquare = p;
+
+}
+
+public int getRank() {
+
+return rank;
+
+}
+
+public int getFile() {
+
+return file;
+
+}
+
+public String getColor() {
+
+return Color;
+
+}
+
+public boolean getOccupancy() {
+
+return isOccupied;
+
+}
+
+//Just in case we need to know exactly what piece is on the square
+
+public Piece getPieceOnSquare() {
+
+//Can't know what piece is on the square if the square isn't occupied
+
+if(!isOccupied) {
+
+throw new IllegalStateException("Square isn't occupied!");
+
+}
+
+return onSquare;
+
+}
+
+public void occupySquare(Piece p) {
+
+//Can't occupy a square that is already occupied
+
+if(isOccupied) {
+
+throw new IllegalStateException("Square already occupied!");
+
+}
+
+onSquare = p;
+
+isOccupied = true;
+
+}
+
+public void unOccupySquare() {
+
+//Can't unoccupy a square that is already unoccupied
+
+if(!isOccupied) {
+
+throw new IllegalStateException("Square already unoccupied!");
+
+}
+
+onSquare = null;
+
+isOccupied = false;
+
+}
+
+public double getTotalDistance(Square destination) {
+
+//Using typical distance formula, storing signed
+
+return Math.sqrt((this.rank - destination.getRank())^2 + (this.file - destination.getFile())^2);
+
+}
+
+//We can just convert both to an absolute value to make the legality check easier.
+
+public int getRankDistance(Square destination) {
+
+return Math.abs(this.rank - destination.getRank());
+
+}
+
+//Will have to check for pawn legality seperately
+
+public int getFileDistance(Square destination) {
+
+return Math.abs(this.file - destination.getFile());
+
+}
+
+}
